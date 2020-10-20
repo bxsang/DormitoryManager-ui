005e948cbf5704622cd40faf71b8584bb0154d10
Sang Bui <xsang.bui@gmail.com>
2020-10-21 04:42:14 +0700

    Attendance: import csv and upload

--- src/components/Attendance.vue
+++ src/components/Attendance.vue
@@ -30,6 +30,15 @@
             >
               <em class="fas fa-download mt-0" />
             </mdb-btn>
+            <mdb-btn
+              outline="white"
+              rounded
+              size="sm"
+              class="px-2"
+              @click.native.prevent="upload_modal = true"
+            >
+              <em class="fas fa-upload mt-0" />
+            </mdb-btn>
           </div>
           <a
             class="white-text mx-3"
@@ -50,6 +59,96 @@
         </div>
       </div>
     </mdb-container>
+    <mdb-modal
+      :show="upload_modal"
+      @close="upload_modal = false"
+      cascade
+      class="text-left"
+    >
+      <mdb-modal-header class="primary-color white-text">
+        <h4 class="title">
+          <em class="fas fa-pencil-alt" /> Upload file
+        </h4>
+      </mdb-modal-header>
+      <mdb-modal-body class="grey-text">
+        <mdb-input
+          size="sm"
+          group
+          type="file"
+          @change="importLocalFile"
+        />
+        <textarea
+          type="text"
+          id="defaultFormContactMessageEx"
+          class="form-control"
+          rows="3"
+          v-model="attendance_bulk_text"
+        />
+      </mdb-modal-body>
+      <mdb-modal-footer>
+        <mdb-btn
+          color="secondary"
+          @click.native="upload_modal = false"
+        >
+          Đóng
+        </mdb-btn>
+        <mdb-btn
+          color="primary"
+          @click.native.prevent="addAttendanceFile"
+        >
+          Upload
+        </mdb-btn>
+      </mdb-modal-footer>
+    </mdb-modal>
+    <mdb-modal
+      :show="success_modal"
+      @close="success_modal = false"
+      success
+    >
+      <mdb-modal-header>
+        <mdb-modal-title>Success</mdb-modal-title>
+      </mdb-modal-header>
+      <mdb-modal-body class="text-center">
+        <mdb-icon
+          icon="check"
+          size="4x"
+          class="mb-3 animated rotateIn"
+        />
+      </mdb-modal-body>
+      <mdb-modal-footer center>
+        <mdb-btn
+          outline="success"
+          @click.native="success_modal = false"
+        >
+          Đóng
+        </mdb-btn>
+      </mdb-modal-footer>
+    </mdb-modal>
+    <mdb-modal
+      :show="failed_modal"
+      @close="failed_modal = false"
+      danger
+    >
+      <mdb-modal-header>
+        <mdb-modal-title>Failed</mdb-modal-title>
+      </mdb-modal-header>
+      <mdb-modal-body class="text-center">
+        <mdb-icon
+          icon="times"
+          size="4x"
+          class="mb-3 animated rotateIn"
+        />
+        <p>{{ message }}</p>
+      </mdb-modal-body>
+      <mdb-modal-footer center>
+        <mdb-btn
+          outline="danger"
+          @click.native="failed_modal = false"
+        >
+          Đóng
+        </mdb-btn>
+      </mdb-modal-footer>
+    </mdb-modal>
   </div>
 </template>
 
@@ -70,9 +169,12 @@ export default {
       columns: [],
       rows: [],
       selected: null,
+      upload_modal: false,
+      attendance_bulk: [],
+      attendance_bulk_text: '',
+      success_modal: false,
+      failed_modal: false,
       message: '',
-      result: '',
-      result_modal: false,
     };
   },
   computed: {
@@ -98,7 +200,7 @@ export default {
     },
     mapAttendance(json) {
       this.attendance_json = json;
-      const keys = ['status', 'student_id'];
+      const keys = ['date', 'status', 'student_id'];
       const entries = this.filterData(json, keys);
       // columns
       this.columns = keys.map((key) => ({
@@ -114,6 +216,18 @@ export default {
         .then((json) => this.mapAttendance(json))
         .catch((err) => console.log(err));
     },
+    addAttendanceFile() {
+      AttendanceService.addAttendance(this.attendance_bulk)
+        .then(() => {
+          this.add_modal = false;
+          this.success_modal = true;
+          this.refreshRooms();
+        })
+        .catch((error) => {
+          this.message = `Error: ${error}`;
+          this.failed_modal = true;
+        });
+    },
     exportCSVFile() {
       const config = {
         delimiter: ',',
@@ -121,6 +235,26 @@ export default {
       };
       const csv = this.$papa.unparse(this.attendance_json, config);
       this.$papa.download(csv, `attendance_${this.attendance.date}`);
+    },
+    async importLocalFile() {
+      // eslint-disable-next-line no-restricted-globals
+      const file = event.target.files[0];
+      const data = await new Promise((success) => {
+        this.$papa.parse(file, {
+          header: true,
+          complete(results) {
+            const result = results.data;
+            if (result) {
+              success(result);
+            }
+          },
+        });
+      });
+      this.attendance_bulk_text = JSON.stringify(data);
+      data.forEach(this.bindCSV);
+    },
+    bindCSV(value) {
+      this.attendance_bulk.push(value);
     },
     onDateChange(date) {
       localStorage.setItem('date', date);
--- src/services/attendance.js
+++ src/services/attendance.js
@@ -5,6 +5,15 @@ class AttendanceService {
   getAttendance(attendance) {
     return Repository.get(`/attendance/${attendance.date}`);
   }
+
+  async addAttendance(attendance) {
+    const endpoint = `/attendance/${localStorage.getItem('date')}`;
+    const response = await Repository.post(endpoint, attendance);
+    if (!response.success) {
+      throw new Error(response.message);
+    }
+    return response;
+  }
 }
 
 export default new AttendanceService();
